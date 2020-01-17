e33be2eba269129c391624ff9ff40439b7151414
yuudaihasegawa <yuudaihasegawa0218@gmail.com>
2020-01-17 08:53:05 +0000

    public側　user post views 整理

--- db/schema.rb
+++ db/schema.rb
@@ -71,8 +71,6 @@ ActiveRecord::Schema.define(version: 2020_01_09_100436) do
     t.text "content"
     t.integer "price", default: 0
     t.string "address"
-    t.float "latitude"
-    t.float "longitude"
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
